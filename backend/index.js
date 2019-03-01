a9d74fb38cc95bf48ff96fbb26ef618f54a9e018
Connor McCauley <cdmc13@outlook.com>
2019-03-01 16:04:50 -0500

    Fixed residual router error

--- backend/index.js
+++ backend/index.js
@@ -9,18 +9,18 @@ var bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 var mongo = require('mongodb');
 var port = process.env.PORT || 8080;        // set our port
-var router = express.Router();
+//var router = express.Router();
 const serveIndex = require('serve-index');
 var path = require('path');
 
 //var conn = mongoose.createConnection("mongodb://localhost");
 
 // Set routes
-var loginRoutes  = require('./app/routes/login');
-var portalRoutes = require('./app/routes/portal');
+var loginRoutes  = require('../frontend/login');
+var portalRoutes = require('../frontend/portal');
 
 app.use(function (request, response, next) {
-    console.log('init middleware');
+    console.log('Time: ' + Date.now());
     next();
 });
 
@@ -44,8 +44,8 @@ app.set('view engine', 'ejs');
 //mongoose.connect('mongodb://localhost/VIM');
 
 // Use Express to handle routes
-app.use('/login', loginRoutes);
-app.use('/portal', portalRoutes);
+//app.use('/login', loginRoutes);
+//app.use('/portal', portalRoutes);
 
 
 // START THE SERVER
