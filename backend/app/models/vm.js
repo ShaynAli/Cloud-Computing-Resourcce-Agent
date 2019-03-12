8c49106221da17d2ae0a80dae84ba7494d0d23c5
ShaynAli <shayaan.syed.ali@gmail.com>
2019-03-12 16:33:07 -0400

    Backend-frontend compatibility changes

--- backend/app/models/vm.js
+++ backend/app/models/vm.js
@@ -1,13 +1,12 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var event = require('../classes/event');
 
 var VMSchema = new Schema({
     VM_ID: String,
     CC_ID: String,
     vmConfig: String,
     vmStatus: String,
-    event: [event],  //holds the event class
+    event: [],  //holds the event class
     timeOfLastEvent: String,
     totalCharges: String,
     totalUsage: String
